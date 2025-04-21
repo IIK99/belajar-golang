5d26acb5ef597f845357a5aba5d953f1f536e755
IIK99 <muhamad.ikmal1099@gmail.com>
2025-04-21 17:15:04 +0700

    pusing custom err

--- 46error-custom.go
+++ 46error-custom.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type ValidationError struct {
 	Message string
 }
@@ -16,6 +18,42 @@ func (n *NotFoundError) Error() string {
 	return n.Message
 }
 
+func SaveData(id string, data any) error {
+	if id == "" {
+		return &ValidationError{Message: "ID tidak boleh kosong"}
+	}
+
+	if id != "Iik" {
+		return &NotFoundError{Message: "Data tidak ditemukan"}
+	}
+
+	// Jika oke
+	return nil
+}
+
 func main() {
-	
+	err := SaveData("Iik", nil) // ID kosong, random, Iik = data tidak ditemukan
+	if err != nil {
+		// // terjadi error
+		// if ValidationError, ok := err.(*ValidationError); ok {
+		// 	fmt.Println("Validation Error:", ValidationError.Error())
+		// } else if NotFoundError, ok := err.(*NotFoundError); ok {
+		// 	fmt.Println("Not Found Error:", NotFoundError.Error())
+		// } else {
+		// 	fmt.Println("Unknown Error:", err.Error())
+		// }
+
+		// menggunakan switch case
+		switch finalErr := err.(type) {
+			case *ValidationError:
+				fmt.Println("Validation Error:", finalErr.Error())
+			case *NotFoundError:
+				fmt.Println("Not Found Error:", finalErr.Error())
+			default:
+				fmt.Println("Unknown Error:", finalErr.Error())
+		}
+	} else {
+		// sukses
+		fmt.Println("Data berhasil disimpan")
+	}
 }
