0d2751209363d0a40947f746603391f3f809cdae
IIK99 <muhamad.ikmal1099@gmail.com>
2025-06-02 08:16:41 +0700

    file manipulation

--- golang-standard-library/23file.go
+++ golang-standard-library/23file.go
@@ -2,7 +2,12 @@
 
 package main
 
-import "os"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
 
 func createNewFile(name string, message string) error {
 	file, err := os.OpenFile(name, os.O_CREATE|os.O_WRONLY, 0666)
@@ -18,8 +23,49 @@ func createNewFile(name string, message string) error {
 	return nil
 }
 
+func addToFile(name string, message string) error {
+	file, err := os.OpenFile(name, os.O_RDWR|os.O_APPEND, 0666)
+	if err != nil {
+		return err
+	}
+	defer file.Close() 
+	file.WriteString(message) 
+	return nil
+}
+
+func readFile(name string) (string, error) {
+	file, err := os.OpenFile(name, os.O_RDONLY, 0666)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close() // pastikan file ditutup setelah selesai digunakan
+
+	reader := bufio.NewReader(file)
+	var message string 
+	for {
+		line, _, err := reader.ReadLine() // membaca baris demi baris
+		if err == io.EOF {
+			break // jika sudah mencapai akhir file, keluar dari loop
+		}
+		message += string(line) + "\n" // menambahkan baris yang dibaca ke message
+	}
+	return message, nil // mengembalikan pesan yang dibaca dari file
+	// fungsi ini akan membaca file dengan nama yang diberikan dan mengembalikan isi file sebagai string.
+}
+
 func main() {
-	createNewFile("example.txt", "Hello, World!\nThis is a new file created using os.OpenFile.\n")
+	// Contoh penggunaan fungsi createNewFile
+	// createNewFile("example.txt", "Hello, World!\nThis is a new file created using os.OpenFile.\n")
 	// Fungsi createNewFile akan membuat file baru dengan nama "example.txt" dan menulis pesan ke dalamnya.
 	// bisa juga penamaan file nya "example.log" atau "example.csv" atau "example.json" sesuai kebutuhan.
+	
+	// Contoh penggunaan fungsi addToFile
+	addToFile("example.txt", "This is an additional line added to the file.\n")
+	
+	// Contoh penggunaan fungsi readFile
+	result, _ := readFile("example.txt")
+	// Fungsi readFile akan membaca file "example.txt" dan mengembalikan isi file tersebut sebagai string.
+	fmt.Println("Isi file:", result)
+	// Jika file tidak ditemukan, akan mengembalikan error. Tapi error nya saya ignore dengan menggunakan underscore (_).
+
 }
