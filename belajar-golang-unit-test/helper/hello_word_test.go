6b3ce2149e866a13852e28143b316d51cc40b95c
IIK99 <muhamad.ikmal1099@gmail.com>
2026-01-23 18:05:18 +0700

    require and assert

--- belajar-golang-unit-test/helper/hello_word_test.go
+++ belajar-golang-unit-test/helper/hello_word_test.go
@@ -3,11 +3,12 @@ package helper
 import (
 	"fmt"
 	"testing"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 // wajib nama function nya Test
 // harus memiliki parameter (t *testing.T) dan tidak boleh ada return value
-
 
 func TestHelloWord(t *testing.T) {
 	result := HelloWord("Iik")
@@ -34,8 +35,32 @@ func TestHelloWordIkmal(t *testing.T) {
 // cara run di terminal helper
 // go test = run semua test di folder helper
 // go test -v = run semua test function
-// go test -run TestHelloWordIkmal = run test function TestHelloWordIkmal
 
 // perbedaan t.Error dan t.Fatal
 // t.Error = error tapi test function tetap lanjut
 // t.Fatal = error dan test function langsung berhenti
+
+// assertion = pengecekan apakah hasil sesuai dengan yang diharapkan
+// install library/kunjungi assertion = go get github.com/stretchr/testify
+
+func TestHelloWorldRequire(t *testing.T) {
+	result := HelloWord("Iik")
+	require.Equal(t, "Hello Iik", result, "Result must be 'Hello Iik'")
+	// t, expected, actual, message
+	fmt.Print("Test Hello with require is success")
+}
+
+func TestHelloWorldAssert(t *testing.T) {
+	result := HelloWord("Iik")
+	assert.Equal(t, "Hello Iik", result, "Result must be 'Hello Iik'")
+	// t, expected, actual, message
+	fmt.Print("Test Hello with assert is success")
+}
+
+// go test -v -run TestHelloWorldRequire
+// go test -v -run TestHelloWorldAssert
+
+// perbedaan require dan assert
+// require = error dan test function langsung berhenti
+// assert = error tapi test function tetap lanjut
+
