71a5e8d9dcbb7bfb376b117cd3fd3a7392eaaa22
IIK99 <muhamad.ikmal1099@gmail.com>
2026-01-23 17:31:39 +0700

    fail test

--- belajar-golang-unit-test/helper/hello_word_test.go
+++ belajar-golang-unit-test/helper/hello_word_test.go
@@ -1,6 +1,9 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 // wajib nama function nya Test
 // harus memiliki parameter (t *testing.T) dan tidak boleh ada return value
@@ -13,6 +16,8 @@ func TestHelloWord(t *testing.T) {
 		// error
 		t.Error("Result must be 'Hello Iik'")
 	}
+
+	fmt.Print("Ini menggunakan t.Error")
 }
 
 func TestHelloWordIkmal(t *testing.T) {
@@ -20,11 +25,17 @@ func TestHelloWordIkmal(t *testing.T) {
 
 	if result != "Hello Ikmal" {
 		// error
-		t.Error("Result must be 'Hello Ikmal'")
+		t.Fatal("Result must be 'Hello Ikmal'")
 	}
+
+	fmt.Print("Ini menggunakan t.Fatal")
 }
 
 // cara run di terminal helper
 // go test = run semua test di folder helper
 // go test -v = run semua test function
 // go test -run TestHelloWordIkmal = run test function TestHelloWordIkmal
+
+// perbedaan t.Error dan t.Fatal
+// t.Error = error tapi test function tetap lanjut
+// t.Fatal = error dan test function langsung berhenti
